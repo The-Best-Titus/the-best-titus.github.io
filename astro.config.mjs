7e9b7b1abc0dea9de305f6acb0625244b993c36b
The-Best-Titus <tmaunu2@unl.edu>
2025-11-19 17:31:31 -0600

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import tailwindcss from '@tailwindcss/vite';
 import mdx from '@astrojs/mdx';
 
 // https://astro.build/config
+site: 'https://The-Best-Titus.github.io',
+base: '/portfolio', // Your repo name
 export default defineConfig({
   integrations: [mdx()],
   vite: {
