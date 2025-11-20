acb88a497e906d743a8c2767d959876d4e759126
The-Best-Titus <tmaunu2@unl.edu>
2025-11-19 19:27:54 -0600

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import mdx from '@astrojs/mdx';
 
 export default defineConfig({
   site: 'https://the-best-titus.github.io',
-  base: '/bloomfolio',
+  base: '/portfolio',
   trailingSlash: 'ignore',
   integrations: [mdx()],
   vite: {
